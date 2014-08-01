32b4f6dbe636a4ddcfa630f0e79a1fb3514d0f54
Dan Pallas <github@danpallas.com>
2014-07-31 22:18:18 -0400

    deleted actual files

--- README.md
+++
@@ -1,9 +0,0 @@
-green-tags
-==========
-
-Audio tagging library for Clojure
-
-The goal of this library is provide functionality for getting and setting tags from mp3, mp4/m4a, ogg, and flac files
-in a simple, clojure-friendly way that smooths over all the differences between the different tag types.
-green-tags is currently in alpha and only supports reading of standard tags. I'm actively working on expanding
-functionalityl. 
--- project.clj
+++
@@ -1,9 +0,0 @@
-(defproject green-tags "0.2.0-alpha"
-  :description "An audio tagging library supporing mp3 mp4 flac and ogg. It is intended to be useable with the with little boiler plate code."
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org/jaudiotagger "2.0.3"]]
-  :profiles {:dev {:dependencies [[midje "1.6.3"]]}}
-  :plugins [[codox "0.8.10"]])
